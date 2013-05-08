c4d614df45dd34830f6111a87d483fda1b1d9df2
mo khan <mo@mokhan.ca>
2013-05-07 18:35:04 -0600

    initial commit

---
+++ 2013-05-07/My First App/My First App/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My First App
+//
+//  Created by mo khan on 2013-05-07.
+//  Copyright (c) 2013 mo khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
